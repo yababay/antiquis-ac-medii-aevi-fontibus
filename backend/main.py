284f70f000914e364a6b8ce4b314320b4ae38848
Михаил Беляков <bigbelk@live.ru>
2022-01-21 11:44:12 +0300

    ../frontend/docs

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 HTTP_HOST = os.getenv('HTTP_HOST', 'localhost')
 HTTP_PORT = os.getenv('HTTP_PORT', '8080')
-HTTP_ROOT = os.getenv('HTTP_ROOT', 'public')
+HTTP_ROOT = os.getenv('HTTP_ROOT', '../frontend/docs')
 
 if __name__ == '__main__':
     setup_aiohttp(host=HTTP_HOST, port=HTTP_PORT, static_dir=HTTP_ROOT, routes=routes)
